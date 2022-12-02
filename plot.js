4423e3377c49ebf1ba0223aa448be7b29a3c699d
asikarov <37193177+asikarov@users.noreply.github.com>
2022-12-02 17:58:28 -0500

    updating color

--- plot.js
+++ plot.js
@@ -33,9 +33,9 @@ svg.append("g")
 .call(d3.axisLeft(y));
 
 // color for dots
-var myScale = d3.scaleThreshold()
-	.domain([1000, 2000, 3000])
-	.range(["red", 'yellow', 'lightblue', "green"]);
+var colorScale1 = d3.scaleLinear()
+	.domain([0, 4000])
+	.range(["#FF0000", "#00FF00"]);
 
 // Add dots
 svg.append('g')
@@ -45,7 +45,43 @@ svg.append('g')
 .attr("cx", function (d) { return x(d.GrLivArea); } )
 .attr("cy", function (d) { return y(d.SalePrice); } )
 .attr("r", 1.5)
-.style('fill', function(d) {
-    return myScale(d.GrLivArea);
-});
+.style('fill', function(d) {return colorScale1(d.GrLivArea); });
 })
+
+//recolor dots
+function update(data) {
+    console.log("updating...");
+    //Read the data
+    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv").then( function(data) {
+
+    // Add X axis
+    const x = d3.scaleLinear()
+    .domain([0, 4000])
+    .range([ 0, width ]);
+    svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+    
+    // Add Y axis
+    const y = d3.scaleLinear()
+    .domain([0, 500000])
+    .range([ height, 0]);
+    svg.append("g")
+    .call(d3.axisLeft(y));
+    
+    //new colors
+    var colorScale2 = d3.scaleLinear()
+        .domain([0, 4000])
+        .range(["#00FF00", "#FF0000"]);
+    
+    // Add dots
+    svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .join("circle")
+    .attr("cx", function (d) { return x(d.GrLivArea); } )
+    .attr("cy", function (d) { return y(d.SalePrice); } )
+    .attr("r", 1.5)
+    .style('fill', function(d) {return colorScale2(d.GrLivArea); });
+    })
+}
